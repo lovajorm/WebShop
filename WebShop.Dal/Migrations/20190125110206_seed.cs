ba86d78a4d046bac129b863c1ba7feb911d4635a
Patric Linderoth <patric.linderoth@tfbank.se>
2019-01-25 13:17:04 +0100

    Seed Database

---
+++ WebShop.Dal/Migrations/20190125110206_seed.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebShop.Dal.Migrations
+{
+    public partial class seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductID", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Beskrivning av produkt 1", "", 10f, "Produkt 1" },
+                    { 2, "Beskrivning av objekt 2", "", 20f, "Produkt 2" },
+                    { 3, "Beskrivning av objekt 3", "", 30f, "Produkt 3" },
+                    { 4, "Beskrivning av objekt 4", "", 40f, "Produkt 4" },
+                    { 5, "Beskrivning av objekt 5", "", 50f, "Produkt 5" },
+                    { 6, "Beskrivning av objekt 6", "", 60f, "Produkt 6" },
+                    { 7, "Beskrivning av objekt 7", "", 70f, "Produkt 7" },
+                    { 8, "Beskrivning av objekt 8", "", 80f, "Produkt 8" },
+                    { 9, "Beskrivning av objekt 9", "", 90f, "Produkt 9" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductID",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductID",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductID",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductID",
+                keyValue: 9);
+        }
+    }
+}
--- WebShop.Dal/WebShopDbContext.cs
+++ WebShop.Dal/WebShopDbContext.cs
@@ -9,6 +9,82 @@ namespace WebShop.Dal
         public DbSet<Product> Products { get; set; }
 
         public WebShopDbContext(DbContextOptions<WebShopDbContext> context) : base(context) {}
-    }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product()
+                {
+                    ProductID = 1,
+                    Title = "Produkt 1",
+                    Description = "Beskrivning av produkt 1",
+                    Price = 10,
+                    ImageUrl = ""
+                },
+                new Product()
+                {
+                    ProductID = 2,
+                    Title = "Produkt 2",
+                    Description = "Beskrivning av objekt 2",
+                    Price = 20,
+                    ImageUrl = ""
+                },
+                new Product()
+                {
+                    ProductID = 3,
+                    Title = "Produkt 3",
+                    Description = "Beskrivning av objekt 3",
+                    Price = 30,
+                    ImageUrl = ""
+                },
+                new Product()
+                {
+                    ProductID = 4,
+                    Title = "Produkt 4",
+                    Description = "Beskrivning av objekt 4",
+                    Price = 40,
+                    ImageUrl = ""
+                },
+                new Product()
+                {
+                    ProductID = 5,
+                    Title = "Produkt 5",
+                    Description = "Beskrivning av objekt 5",
+                    Price = 50,
+                    ImageUrl = ""
+                },
+                new Product()
+                {
+                    ProductID = 6,
+                    Title = "Produkt 6",
+                    Description = "Beskrivning av objekt 6",
+                    Price = 60,
+                    ImageUrl = ""
+                },
+                new Product()
+                {
+                    ProductID = 7,
+                    Title = "Produkt 7",
+                    Description = "Beskrivning av objekt 7",
+                    Price = 70,
+                    ImageUrl = ""
+                },
+                new Product()
+                {
+                    ProductID = 8,
+                    Title = "Produkt 8",
+                    Description = "Beskrivning av objekt 8",
+                    Price = 80,
+                    ImageUrl = ""
+                },
+                new Product()
+                {
+                    ProductID = 9,
+                    Title = "Produkt 9",
+                    Description = "Beskrivning av objekt 9",
+                    Price = 90,
+                    ImageUrl = ""
+                });
+        }
+    }
 }

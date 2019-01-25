ef9feea8e0b6660479e24a4097e519cfa290c354
Lova Jormfeldt <Lova.Jormfeldt@tfbank.se>
2019-01-25 11:25:11 +0100

    addedProductTitle

--- WebShop.Bo/Product.cs
+++ WebShop.Bo/Product.cs
@@ -5,6 +5,7 @@ namespace WebShop.Bo
     public class Product
     {
         public int ProductID { get; set; }
+        public string Title { get; set; }
         public string Description { get; set; }
         public float Price { get; set; }
         public string ImageUrl { get; set; }
---
+++ WebShop.Dal/Migrations/20190125102031_addedProductTitle.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebShop.Dal.Migrations
+{
+    public partial class addedProductTitle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Products",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Products");
+        }
+    }
+}

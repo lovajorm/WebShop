706ac29e9a2bb8685bbd42b25f6b7edeaf7861ec
Daniel Jareteg <daniel.jareteg@tfbank.se>
2019-01-29 12:52:41 +0100

    Removed Dead code

--- WebShop/Startup.cs
+++ WebShop/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -54,7 +50,7 @@ namespace WebShop
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/WebShop/Error");
             }
 
             app.UseStaticFiles();
@@ -65,7 +61,7 @@ namespace WebShop
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=WebShop}/{action=Index}/{id?}");
             });
         }
     }

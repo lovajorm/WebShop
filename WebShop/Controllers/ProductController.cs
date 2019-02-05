5ce30e730ce95cc3405b6f7ce1ce6e33187dc603
Patric Linderoth <patric.linderoth@tfbank.se>
2019-02-05 08:16:23 +0100

    LoggingDraft

--- WebShop/Controllers/ProductController.cs
+++ WebShop/Controllers/ProductController.cs
@@ -2,31 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using log4net.Core;
+using log4net.Repository.Hierarchy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebShop.Bo;
 using WebShop.Dal;
+using WebShop.Log;
 using WebShop.Web.Interfaces;
 using WebShop.Web.Repositories;
 using WebShop.Web.ViewModels;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
+using ILoggerFactory = Microsoft.Extensions.Logging.ILoggerFactory;
 
 namespace WebShop.Web.Controllers
 {
     public class ProductController : Controller
     {
         private readonly WebShopDbContext _context;
+        private readonly IMessageLogger _logger;
         private readonly ICategoryRepository _categoryRepository;
         private readonly IProductRepository _productRepository;
 
-        public ProductController(ICategoryRepository categoryRepository, IProductRepository productRepository, WebShopDbContext context)
+        public ProductController(ICategoryRepository categoryRepository, IProductRepository productRepository, WebShopDbContext context, IMessageLogger logger)
         {
             _categoryRepository = categoryRepository;
             _productRepository = productRepository;
             _context = context;
+            _logger = logger;
         }
 
         public ViewResult List(string category)
         {
+            //_logger.TestLog(this.GetType().Name);
+            //_logger.TestLog(this.GetType().Name, LoggingLevel.Info);
+            _logger.LogInfo(this.GetType().Name);
+
             string _category = category;
             IEnumerable<Product> products;
             string currentCategory = string.Empty;
--- WebShop/Startup.cs
+++ WebShop/Startup.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using log4net;
+using log4net.Config;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -6,7 +10,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using WebShop.Dal;
+using WebShop.Log;
 using WebShop.Web.Interfaces;
 using WebShop.Web.Models;
 using WebShop.Web.Repositories;
@@ -31,6 +37,15 @@ namespace WebShop
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("C:\\Development\\Webshoppen\\WebShop\\WebShop.Log\\log4netCustom.config"));
+            services.AddSingleton<IMessageLogger, MessageLogger>();
+
+            //services.AddSingleton(new LoggerFactory()
+            //    .AddConsole(Configuration.GetSection("Logging"))
+            //    .AddDebug()
+            //    .AddLog4Net(Configuration.GetValue<string>("Log4NetConfigFile:Name")));
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped(sp => ShoppingCart.GetCart(sp));

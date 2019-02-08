6a9cf0153f3236038d2145b613deaa77c7c316d2
Lova Jormfeldt <Lova.Jormfeldt@tfbank.se>
2019-02-08 16:44:18 +0100

    connectionhandler

--- WebShop/Controllers/OrderController.cs
+++ WebShop/Controllers/OrderController.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 using WebShop.Avarda.Api;
 using WebShop.Avarda.Api.Avarda;
 using WebShop.Bo;
@@ -15,17 +10,16 @@ using WebShop.Web.Models;
 namespace WebShop.Web.Controllers
 {
     public class OrderController : Controller
-
     {
         private readonly IOrderRepository _orderRepository;
         private readonly ShoppingCart _shoppingCart;
-        private GetCustomer _getCustomer;
+        private ConnectionHandler _getCustomer;
 
         public OrderController(IOrderRepository orderRepository, ShoppingCart shoppingCart)
         {
             _orderRepository = orderRepository;
             _shoppingCart = shoppingCart;
-            _getCustomer = new GetCustomer();
+            _getCustomer = new ConnectionHandler();
         }
 
         [HttpGet]
@@ -41,15 +35,29 @@ namespace WebShop.Web.Controllers
             {
                 return Json(new ErrorViewModel { ErrorMessage = $"Failed to get customer. Error: {ex.Message}" });
             }
-
         }
 
         [HttpPost] //send authorization to web api
-        public async Task<IActionResult> AuthorizeInvoice(InvoiceRequest request, Order order)
+        public IActionResult AuthorizeInvoice(InvoiceRequest request, Order order)
         {
+            var total = _shoppingCart.GetShoppingCartTotal();
+            order.OrderTotal = total;
+
             try
             {
-                var invoice = _getCustomer.Au
+                var response = _getCustomer.AuthorizeInvoice(request, order);
+
+                if (total < response.Result.CreditLimit)//In tests the creditLimit is not fixed, which means that the if-statement is unnecessary but we will keep it for fun.
+                {
+                    //Save order and take customer to final page
+                    order.OrderDetails = _orderRepository.CreateOrder(order);
+                    _shoppingCart.ClearCart();//Clears cart after "Complete order"
+                    return View("CheckoutComplete", order);
+                }
+                else
+                {
+                    return View("Error", new ErrorViewModel { ErrorMessage = $"Your credit score is too low" });
+                }
             }
             catch (Exception ex)
             {
@@ -57,16 +65,15 @@ namespace WebShop.Web.Controllers
             }
         }
 
-        public IActionResult Checkout()                         //"Check out" from shopping cart to information form.
+        public IActionResult Checkout()//"Check out" from shopping cart to information form.
         {
             var items = _shoppingCart.GetShoppingCartItems();
             _shoppingCart.ShoppingCartItems = items;
-
-            if (_shoppingCart.ShoppingCartItems.Count == 0)                     //Check to see if the shopping cart contains any items.
+            
+            if (_shoppingCart.ShoppingCartItems.Count == 0)//Check to see if the shopping cart contains any items.
             {
-                return View("Error", new ErrorViewModel { ErrorMessage = "Your cart is empty, add some products first" });                  //Error message shown if you try to check out order without any items in the cart.
+                return View("Error", new ErrorViewModel { ErrorMessage = "Your cart is empty, add some products first" }); //Error message shown if you try to check out order without any items in the cart.
             }
-
             return View();
         }
     }

3092577ed19f1a271b38ce044125760d4578024f
Lova Jormfeldt <Lova.Jormfeldt@tfbank.se>
2019-02-12 16:18:18 +0100

    GetPaymentStatus and cleaned up code

---
+++ WebShop.Avarda.Api/Avarda/PaymentMethodEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebShop.Avarda.Api.Avarda
+{
+    public enum PaymentMethodEnum
+    {
+        Invocie = 0,
+        Loan = 1,
+        Card = 2,
+        DirectPayment = 3,
+    }
+}
--- WebShop.Avarda.Api/ConnectionHandler.cs
+++ WebShop.Avarda.Api/ConnectionHandler.cs
@@ -21,6 +21,47 @@ namespace WebShop.Avarda.Api
             };
         }
 
+        public PaymentStatus GetPaymentStatus(string paymentId)
+        {
+            PaymentStatus response = null;
+            var request = new PaymentResponse()
+            {
+                PurchaseId = paymentId
+            };
+            using (var handler = new WebRequestHandler())
+            {
+                using (var client = new HttpClient(handler))
+                {
+                    var bytearray = Encoding.ASCII.GetBytes("TestSweden1:test1");               //Authentication
+
+                    //sets authentication header.
+                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(bytearray));
+
+                    client.BaseAddress = new Uri($"https://stage.avarda.org/");
+
+                    //Serializes the class to json
+                    
+                    var body = JsonConvert.SerializeObject(request);
+                    var requestBody = new StringContent(body, Encoding.UTF8, "application/json");
+                    var result = client.PostAsync("CheckOut2/CheckOut2Api/GetPaymentStatus", requestBody).Result;                                          //Calling Avarda API and sending the json
+
+                    if (!result.IsSuccessStatusCode)
+                    {
+                        throw new Exception(result.Content.ReadAsStringAsync().Result);
+                    }
+
+                    //response.PurchaseId = result.Content.ReadAsStringAsync().Result;
+
+                    if (result.IsSuccessStatusCode)
+                    {
+                        response = JsonConvert.DeserializeObject<PaymentStatus>(result.Content.ReadAsStringAsync().Result);
+                        
+                    }
+                }
+            }
+            return response;
+        }
+
         public PaymentResponse InitializePayment(PaymentRequest request)
         {
             //var request = new PaymentRequest();
@@ -46,70 +87,16 @@ namespace WebShop.Avarda.Api
                         throw new Exception(result.Content.ReadAsStringAsync().Result);
                     }
 
-                    response.PaymentID = result.Content.ReadAsStringAsync().Result;
+                    response.PurchaseId = result.Content.ReadAsStringAsync().Result;
 
                     if (result.IsSuccessStatusCode)
                     {
-                        response.PaymentID = response.PaymentID.Replace("\"", string.Empty);
+                        response.PurchaseId = response.PurchaseId.Replace("\"", string.Empty);
                         //return View("Avarda", response);
                     }
                 }
             }
             return response;
-        }
-
-        public Customer GetCustomerInfo(string ssn)              //Method which gets customer information by using Ssn, see checkout.cshtml.
-        {
-            Customer response;                         //Initializes customer
-
-            using (var handler = new WebRequestHandler())
-            {
-                using (var client = new HttpClient(handler))
-                {
-                    var bytearray = Encoding.ASCII.GetBytes(_connectionDetails.ToString());
-
-                    //sets authentication header.
-                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(bytearray));
-
-                    //Sends a json-object and gets back result, converts result from json to c# which is "response"
-
-                    var result = client.GetStringAsync(new Uri($"https://stage.avarda.org/WebShopApi/webshop/ssn/swe/{ssn}")).Result;
-                    response = JsonConvert.DeserializeObject<Customer>(result);                  //Converts from json to c# class.
-                }
-            }
-            return response;
-        }
-
-        public async Task<InvoiceResponse> AuthorizeInvoice(InvoiceRequest request, Order order)
-        {
-            using (var handler = new WebRequestHandler())
-            {
-                using (var client = new HttpClient(handler))
-                {
-                    var bytearray = Encoding.ASCII.GetBytes(_connectionDetails.ToString());
-
-                    //sets authentication header.
-                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(bytearray));
-
-                    client.BaseAddress = new Uri($"https://stage.avarda.org/");
-
-                    var total = order.OrderTotal;
-                    request.Amount = total;
-                    request.Country = "Swe";
-                    
-                    //Serializes the class to json
-                    var body = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
-                    var result = await client.PostAsync("WebShopApi/webshop/authorization/invoice", body);                                          //Calling Avarda API and sending the json
-
-                    if (!result.IsSuccessStatusCode)
-                    {
-                        throw new Exception(result.Content.ReadAsStringAsync().Result);
-                    }
-                    var response = JsonConvert.DeserializeObject<InvoiceResponse>(result.Content.ReadAsStringAsync().Result);
-
-                    return response;
-                }
-            }
         }
     }
 }
